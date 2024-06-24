d9e384b5116a58639d4c2bf05e27a6aa7172643f
Arilson-X <arilsonsouzacosta0@gmail.com>
2024-06-24 16:18:06 -0300

    Atualizado estrutura do projeto

--- QrCode/QrCode.py
+++ QrCode/QrCode.py
@@ -11,18 +11,20 @@ class QrCode():
     def create_image(self):
         self.__qr_code = pyqrcode.create(self.url)
         self.name = str(datetime.now().timestamp())
-    
+        self.save_png(temp=True)
+
     def save_svg(self):
         self.__qr_code.svg(
             file= f"Images/svg/{self.name}.svg",
             scale=6,
-            module_color=self.__module_color,
-            background= self.__background_color
+            # module_color=self.__module_color,
+            # background= self.__background_color
         )
 
-    def save_png(self):
+    def save_png(self, temp=False):
+        file_name = f"temp/img/{self.name}.png" if temp else f"Images/png/{self.name}.png"
         self.__qr_code.png(
-            file= f"Images/png/{self.name}.png",
+            file= file_name,
             scale=6,
             module_color=self.__module_color,
             background= self.__background_color
@@ -31,5 +33,6 @@ class QrCode():
     def change_color(self, bg_color, m_color):
         self.__background_color = bg_color
         self.__module_color = m_color
+
 
 
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from QrCode.QrCode import QrCode
+from OsTratative.OsTratative import OsTratative
 
 url = QrCode("https://www.linkedin.com/in/arilson-xavier-0a581a145/","Teste")
 url.create_image()
 url.save_png()
 url.save_svg()
+OsTratative()
